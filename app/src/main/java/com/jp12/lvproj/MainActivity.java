4680adb4156141fffbb5cd9d6c43f464317c4494
Josh Patra <joshpatra12@gmail.com>
2021-12-20 13:54:21 -0500

    more work?

--- app/src/main/java/com/jp12/lvproj/MainActivity.java
+++ app/src/main/java/com/jp12/lvproj/MainActivity.java
@@ -3,11 +3,13 @@ package com.jp12.lvproj;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
@@ -20,6 +22,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,26 +33,47 @@ public class MainActivity extends AppCompatActivity {
     TextView textViewer2;
     TextView textViewer3;
     int orientation;
-    int curPos;
+    Button rem;
+    int curPos = -1;
+    CustomAdapter customAdapter;
+    ConstraintLayout prevObj;
     ArrayList<String> stockSymbols;
+    ArrayList<String> nullStuff;
     ArrayList<String> stockNames;
     ArrayList<String> stockDescriptions;
     ArrayList<Double> stockPrices;
     ArrayList<Drawable> stockImages;
-    final static String KEYEK = "abcd123";
+    ArrayList<Integer> removedList;
+    final static String KEYEK = "hwebqiuerbvoqebovqe";
+    final static String SYMBOLSKEY = "ewfgewvqebqev";
+    final static String REMOVEDKEY = "asashgdashashah";
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt(KEYEK,curPos);
+        outState.putIntegerArrayList(REMOVEDKEY, removedList);
     }
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         Log.d("TAG", "onRestoreInstanceState: asdfasdgasg");
         orientation = getResources().getConfiguration().orientation;
         super.onRestoreInstanceState(savedInstanceState);
-        curPos = savedInstanceState.getInt(KEYEK,0);
+        curPos = savedInstanceState.getInt(KEYEK,-1);
+        nullStuff = new ArrayList<>();
+        nullStuff.add("");
+        removedList = new ArrayList<>();
+        removedList = savedInstanceState.getIntegerArrayList(REMOVEDKEY);
+        for (int a: removedList
+             ) {
+            stockSymbols.remove(a);
+            stockImages.remove(a);
+            stockDescriptions.remove(a);
+            stockNames.remove(a);
+            stockPrices.remove(a);
+        }
         textViewer.setText("Name: "+stockNames.get(curPos));
         textViewer2.setText("Price: "+stockPrices.get(curPos));
+        listView.setAdapter(customAdapter);
         if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
             textViewer3.setText("Description: "+stockDescriptions.get(curPos));
         } else {
@@ -58,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         populateStockSymbols();
@@ -67,16 +92,44 @@ public class MainActivity extends AppCompatActivity {
         populateStockImages();
         orientation = getResources().getConfiguration().orientation;
         listView = findViewById(R.id.id_list_view);
-        CustomAdapter customAdapter = new CustomAdapter(this, R.layout.adapter_custom, stockSymbols);
+        removedList = new ArrayList<>();
+
+        customAdapter = new CustomAdapter(this, R.layout.adapter_custom, stockSymbols);
         listView.setAdapter(customAdapter);
         textViewer = findViewById(R.id.textView);
         textViewer2 = findViewById(R.id.textView2);
         textViewer3 = findViewById(R.id.textView3);
-
+        rem = findViewById(R.id.remButton);
+        rem.setOnClickListener(view -> {
+            if (curPos == -1){
+                Toast.makeText(this, "Select an item", Toast.LENGTH_SHORT).show();
+            }else{
+                stockSymbols.remove(curPos);
+                stockImages.remove(curPos);
+                stockDescriptions.remove(curPos);
+                stockNames.remove(curPos);
+                stockPrices.remove(curPos);
+                removedList.add(curPos);
+                customAdapter.notifyDataSetChanged();
+                textViewer.setText("Name: "+stockNames.get(curPos));
+                textViewer2.setText("Price: "+stockPrices.get(curPos));
+                listView.setAdapter(customAdapter);
+                if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                    textViewer3.setText("Description: "+stockDescriptions.get(curPos));
+                } else {
+                    // In portrait
+                }
+            }
+        });
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (prevObj != null){
+                    prevObj.setBackgroundColor(Color.WHITE);
+                }
+                view.findViewById(R.id.objectified).setBackgroundColor(Color.GRAY);
+                prevObj = view.findViewById(R.id.objectified);
                 curPos = i;
                 textViewer.setText("Name: "+stockNames.get(i));
                 textViewer2.setText("Price: "+stockPrices.get(i));
@@ -92,14 +145,14 @@ public class MainActivity extends AppCompatActivity {
 
     private void populateStockDescripts() {
         stockDescriptions = new ArrayList<>();
-        stockDescriptions.add("The afasfdasasigiuhdsgasoiugsgiuhasdg");
-        stockDescriptions.add("The ahahah");
-        stockDescriptions.add("The zxccbvzxvb");
-        stockDescriptions.add("The weryw");
-        stockDescriptions.add("The bhghjfdyjfjg");
-        stockDescriptions.add("The wetyu");
-        stockDescriptions.add("The cbxcxvb");
-        stockDescriptions.add("The ndnbvcnvbcn");
+        stockDescriptions.add("iRobot is an American company that creates robots intended for consumer use. They are popularly known for their vacuuming line of technology, the Roomba.");
+        stockDescriptions.add("Upwork is a freelancing platform, that allows freelancers to connect to agencies that need their services.");
+        stockDescriptions.add("Fiverr is a site on which normal individuals can hire people with exceptional talents, like performing songs, or making videos, skills that you can easily outsource to fiverr individuals.");
+        stockDescriptions.add("Redfin is a real estate company, on which you can list your home, and upon the house being sold, you pay just 1.0 or 1.5% of the sell price to Redfin");
+        stockDescriptions.add("Beyond Meat is a company specializing in making meat that tastes like real meat, entirely with vegetables. This is a ridiculous feat, as meat is a hard taste to replicate, but so far their research seems to be going well.");
+        stockDescriptions.add("Etsy is an online marketplace, centered in America, that specializes in handmade of vintage items. This can be a good way to get things like custom jewelry, bags, clothes, and furniture suited especially for you.");
+        stockDescriptions.add("Teladoc is a telemedicine company that specializes in ensuring that everyone everywhere has access to medical opinions. They are also branching into AI, and understanding more about how to better diagnose patients.");
+        stockDescriptions.add("");
         stockDescriptions.add("The dfhfdhfdshgfdgh");
         stockDescriptions.add("The cvbn");
         stockDescriptions.add("The kjkhjk");
@@ -110,8 +163,8 @@ public class MainActivity extends AppCompatActivity {
         stockDescriptions.add("The aidwsvihgf");
         stockDescriptions.add("The qeribggv");
         stockDescriptions.add("The qwfdfdoqfe");
-        stockDescriptions.add("The qerwgounqwrotqwoijtqoijwer");
-        stockDescriptions.add("The etwnqewnqrenqenr");
+        stockDescriptions.add("");
+        stockDescriptions.add("Amazon is a global marketplace, on which you can order everything from bedframes to mousepads, all of which will be delivered to your front door in two days time with Amazon Prime. This is one of the largest companies in the world, with a GDP higher than many countries.");
     }
 
     private void populateStockImages() {
@@ -217,18 +270,27 @@ public class MainActivity extends AppCompatActivity {
             mainActivityContext = context;
         }
 
+
         @NonNull
         @Override
         public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
             ImageView imageView;
             TextView textView;
-
             LayoutInflater layoutInflater = (LayoutInflater) mainActivityContext.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
             @SuppressLint("ViewHolder") View v = layoutInflater.inflate(R.layout.adapter_custom, null);
             textView = v.findViewById(R.id.id_adapter_textView);
             textView.setText(stockSymbols.get(position));
             imageView = v.findViewById(R.id.imageView);
             imageView.setImageDrawable(stockImages.get(position));
+            System.out.println(curPos);
+            try {
+                if (curPos == position && curPos != -1){
+                    convertView.findViewById(R.id.objectified).setBackgroundColor(Color.GRAY);
+                }
+            }
+            catch (Exception e){
+                System.out.println(e);
+            }
             return v;
         }
     }
